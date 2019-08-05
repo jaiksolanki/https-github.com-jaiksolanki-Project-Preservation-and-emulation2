098836e35499c46b7eabcd4d60ec094b079e26f5
Jai <jaiksolanki@gmail.com>
2019-08-05 23:44:03 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ mysql = MySQL()
 app = Flask(__name__)
 app.config['MYSQL_DATABASE_USER'] = 'beproject'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Beproject'
-app.config['MYSQL_DATABASE_DB'] = 'dockengine'
+app.config['MYSQL_DATABASE_DB'] = 'dock_engine'
 app.config['MYSQL_DATABASE_HOST'] = 'dockengine.cxd6jyo8ikqn.us-east-1.rds.amazonaws.com'
 mysql.init_app(app)
  
